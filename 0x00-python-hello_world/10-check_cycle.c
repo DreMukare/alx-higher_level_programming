a3b3e33f281f41872dc50656bef7135df843f712
JudgeFudge19 <dremukare@gmail.com>
2021-04-26 18:48:18 +0000

    checking for cycle in linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if singly linked list has a cycle in it
+* @list: linked list
+*
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *current = malloc(sizeof(listint_t));
+
+	current = list;
+	while (current != NULL)
+	{
+		if (current->next == list)
+		{
+			free(current);
+			return (1);
+		}
+		current = current->next;
+	}
+	return (0);
+}
