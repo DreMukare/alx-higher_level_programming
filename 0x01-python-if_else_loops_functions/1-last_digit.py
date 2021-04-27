83e271f71fa35955dddda05ccbe2a4f4b157e844
JudgeFudge19 <dremukare@gmail.com>
2021-04-27 11:56:27 +0000

    generates random int and checks for sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,12 @@ if number < 0:
 else:
     lastDigit = number % 10
 if lastDigit > 5:
-    print('Last digit of {:d} is {:d} and is greater than 5'.format(number, lastDigit))
+    print('Last digit of {:d} is {:d} and is greater '
+    'than 5'.format(number, lastDigit))
 elif lastDigit == 0:
-    print('Last digit of {:d} is {:d} and is 0'.format(number, lastDigit))
+    print('Last digit of {:d} is ' 
+    '{:d} and is 0'.format(number, lastDigit))
 elif lastDigit < 6 and lastDigit != 0:
-        print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, lastDigit))
+        print('Last digit of {:d} '
+	'is {:d} and is less than 6 '
+	'and not 0'.format(number, lastDigit))
