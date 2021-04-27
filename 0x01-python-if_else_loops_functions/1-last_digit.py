0a5bab7fb3726dbafb45092ab8fd9f9f97f705bf
JudgeFudge19 <dremukare@gmail.com>
2021-04-27 11:48:33 +0000

    gets last digit of number and checks for value

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastDigit = abs(number) % 10
+    lastDigit = lastDigit * -1
+else:
+    lastDigit = number % 10
+if lastDigit > 5:
+    print('Last digit of {:d} is {:d} and is greater than 5'.format(number, lastDigit))
+elif lastDigit == 0:
+    print('Last digit of {:d} is {:d} and is 0'.format(number, lastDigit))
+elif lastDigit < 6 and lastDigit != 0:
+        print('Last digit of {:d} is {:d} and is less than 6 and not 0'.format(number, lastDigit))
