5c46222c7fb2d83c3bc50441bb6f3a49a2fae1e1
JudgeFudge19 <dremukare@gmail.com>
2021-04-29 14:51:11 +0000

    playing around with arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+i = 1
+
+if len(argv) == 1:
+    print('0 arguments.')
+elif len(argv) == 2:
+        print('1 argument:')
+        print('1: {}'.format(argv[1]))
+else:
+    print('{:d} arguments:'.format(len(argv) - 1))
+    while i < len(argv):
+        print('{:d}: {:s}'.format(i, argv[i]))
+        i += 1
