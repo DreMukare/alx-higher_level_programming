7a094d51667715bd940bfd9eb22c45297f154fbf
JudgeFudge19 <dremukare@gmail.com>
2021-04-29 13:54:39 +0000

    using imported function

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ a = 5
 
 if __name__ == '__main__':
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-	print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-	print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-	print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
