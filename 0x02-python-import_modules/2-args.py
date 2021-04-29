4902ef0d3bac302557562a0d80521ab800d9b8ea
JudgeFudge19 <dremukare@gmail.com>
2021-04-29 14:56:21 +0000

    playing around with arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 from sys import argv
 i = 1
 
-if len(argv) == 1:
-    print('0 arguments.')
-elif len(argv) == 2:
+if __name__ == '__main__':
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
         print('1 argument:')
         print('1: {}'.format(argv[1]))
-else:
-    print('{:d} arguments:'.format(len(argv) - 1))
-    while i < len(argv):
-        print('{:d}: {:s}'.format(i, argv[i]))
-        i += 1
+    else:
+        print('{:d} arguments:'.format(len(argv) - 1))
+        while i < len(argv):
+            print('{:d}: {:s}'.format(i, argv[i]))
+            i += 1
