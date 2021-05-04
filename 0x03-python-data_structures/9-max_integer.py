ad0906970b31a706fe2ef719f1806c353430a00c
JudgeFudge19 <dremukare@gmail.com>
2021-05-04 16:49:27 +0000

    found max value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_val = my_list[0]
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i > max_val:
+            max_val = i
+    return max_val
