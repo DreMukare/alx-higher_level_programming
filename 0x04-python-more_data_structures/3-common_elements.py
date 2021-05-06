5a2cd4f5578c11d4f1046c08e680f7777b9d582f
JudgeFudge19 <dremukare@gmail.com>
2021-05-06 18:36:26 +0000

    fixed errors

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if len(set_1) == 0 or len(set_2) == 0:
-        return
-    output = set_1 & set_2
-    return output
+    return set_1 & set_2 
