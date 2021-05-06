344fe78c5926a73affb3523fce00f23bbc92ffc4
JudgeFudge19 <dremukare@gmail.com>
2021-05-06 18:34:19 +0000

    fix errors

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if len(my_list) == 0:
-        return None
-    new_list = [replace if x == search else x for x in my_list]
-    return new_list
+    return [replace if x == search else x for x in my_list]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,4 @@ def uniq_add(my_list=[]):
     if len(my_list) == 0:
         return
     new_list = list(set(my_list))
-    output = sum(new_list)
-    return output
+    return sum(new_list)
