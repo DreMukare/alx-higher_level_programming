ae270e3dae2eee25d1fb194a8d8dcea57dff7fc8
JudgeFudge19 <dremukare@gmail.com>
2021-05-06 16:41:06 +0000

    returns set of non common elements present in only one set

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,5 @@
 def common_elements(set_1, set_2):
     if len(set_1) == 0 or len(set_2) == 0:
         return
-    output = list(set_1 & set_2)
+    output = set_1 & set_2
     return output
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    output = set_1 ^ set_2
+    return output
