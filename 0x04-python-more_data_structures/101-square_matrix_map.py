1062468c240b18d826792466b8235d6a6e7c6254
JudgeFudge19 <dremukare@gmail.com>
2021-05-06 19:15:42 +0000

    refactoring code

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    output = matrix[:]
-    return ([list(map(lambda x: x ** 2, i))for i in output])
+    return ([list(map(lambda x: x ** 2, i))for i in matrix])
