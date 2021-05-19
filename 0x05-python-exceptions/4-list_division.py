150b11dd2885dc84ced10a4a221eb9fcd65e89b6
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 17:20:33 +0000

    fixed typo

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,10 +8,11 @@ def list_division(my_list_1, my_list_2, list_length):
             print('wrong type')
             value = 0
         except ZeroDivisionError:
-            print('division from 0')
+            print('division by 0')
             value = 0
         except IndexError:
             print('out of range')
+            value = 0
         finally:
             new_list.append(value)
     return new_list
