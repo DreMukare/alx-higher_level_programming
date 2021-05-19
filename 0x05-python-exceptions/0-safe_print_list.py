6c7a31b09672b5559d8ae32d767fc47aa77333d7
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 16:41:09 +0000

    fixed error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print('{}'.format(my_list[i]), end='')
             a += 1
-        print('')
-        return a
     except:
         pass
+    print()
+    return a
