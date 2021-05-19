287dff117aaf7ea3b91683680d990ea70f17396e
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 17:06:43 +0000

    fixed error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         value = a / b
     except ZeroDivisionError:
-        value = 'None'
+        value = None
     finally:
         print('Inside result: {}'.format(value))
         return value
