a2395bfcbbf281fc986cfbc317331d3ca42efd41
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 16:54:35 +0000

    changed position of code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print('{:d}'.format(my_list[i]), end='')
             output += 1
-    except (TypeError, ValueError):
-        pass
+        except (TypeError, ValueError):
+            pass
     print()
     return output
