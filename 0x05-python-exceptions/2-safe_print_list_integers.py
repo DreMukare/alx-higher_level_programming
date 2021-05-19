c15677def2edd32db06f4dc4a5620fce53ce3397
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 16:50:06 +0000

    index error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print('{:d}'.format(my_list[i]), end='')
-        except (TypeError, ValueError):
+        except (TypeError, ValueError, IndexError):
             pass
         output += 1
     print()
