0d6d1879a450f528c23fa31435b2e668b9b641f2
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 17:34:56 +0000

    added docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+""" Module contains: class Square """
+
+
 class Square:
+    """
+        Square: defines a square
+        Attributes:
+            size (no type or value identification): size of square
+        Method:
+            __init__: init of size attribute for each instance
+    """
+
     def __init__(self, size):
+        """
+            Initialization of attributes for all instances
+            Args:
+                size (no type): size of the square
+        """
+
         self.__size = size
