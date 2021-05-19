7a1d3197b19657dc9b16bbeacc28a4312f524f31
JudgeFudge19 <dremukare@gmail.com>
2021-05-19 16:45:58 +0000

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        a = 0
-        for i in range(x):
+    a = 0
+    for i in range(x):
+        try:
             print('{}'.format(my_list[i]), end='')
-            a += 1
+        a += 1
     except:
         pass
     print()
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,4 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except:
             pass
         output += 1
+    print()
     return output
