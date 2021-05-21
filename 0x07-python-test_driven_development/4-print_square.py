a893294c937acaa940ca572055132b1a126d118e
JudgeFudge19 <dremukare@gmail.com>
2021-05-21 16:26:21 +0000

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" prints a square """
+
+
+def print_square(size):
+    """
+        prints a square
+        Args:
+            size: the size of the square
+    """
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
