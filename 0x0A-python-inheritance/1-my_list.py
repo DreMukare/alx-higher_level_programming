bf7cfd47b6a11e2c376272245af3d9b5a8aa3210
JudgeFudge19 <dremukare@gmail.com>
2021-05-31 19:25:02 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 1-my_list: MyList """
+
+
+class MyList(list):
+    """
+        class MyList
+        inherits from list
+        Methods:
+            print_sorted: prints sorted list
+    """
+    def print_sorted(self):
+        """
+            prints list sorted in ascending order
+        """
+        self.sort()
+        print(self)
