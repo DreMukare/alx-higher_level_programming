2ce90eee837dc100e9c1ed176f76e910c1c7bd3d
JudgeFudge19 <dremukare@gmail.com>
2021-05-31 17:45:31 +0000

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -144,13 +144,13 @@ class Rectangle:
         if not isinstance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
-            raise TypeError('rect_2 must be an instane of Rectangle')
+            raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area == rect_2.area or rect_1.area > rect_2.area:
             return rect_1
         return rect_2
 
     @classmethod
-    def square(cls, size = 0):
+    def square(cls, size=0):
         """
             returns a new rectangle instance with width, height and size
             equal
