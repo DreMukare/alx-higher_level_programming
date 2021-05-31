8f8a91d0942995ea5f01740aec50901c1887713d
JudgeFudge19 <dremukare@gmail.com>
2021-05-31 21:56:04 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+""" 3-to_json_string: def to_json_string """
+
+
+def to_json_string(my_obj):
+    """
+        returns JSON representation of an object
+        Args:
+            my_obj: object to be serialized
+    """
+    return json.dumps(my_obj, sort_keys=True)
