a7a8d3008d8a0b90265827669fac2b61023f0383
JudgeFudge19 <dremukare@gmail.com>
2021-05-31 21:41:26 +0000

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-write_file: def write_file """
+
+
+def write_file(filename="", text=""):
+    """
+        writes a string to a text file (UTF8)
+        Return:
+            no of characters written
+    """
+    with open(filename, 'w') as f:
+        f.write(text)
