2f1466377aff62143ac50a931acfc893f9f440db
JudgeFudge19 <dremukare@gmail.com>
2021-06-01 18:21:27 +0000

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 7-add_item: def add_item """
+import sys
+
+if __name__ == '__main__':
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = 
+        __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    content = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    content = []
+    content.extend(sys.argv)
+
+save_to_json_file(content, 'add_item.json')
