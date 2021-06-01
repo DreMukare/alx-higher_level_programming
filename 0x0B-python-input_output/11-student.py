4a659bc267798d4349448447e16a168ebc4b7fa1
JudgeFudge19 <dremukare@gmail.com>
2021-06-01 19:33:21 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 9-student: class Student """
+
+
+class Student:
+    """
+        class Student
+        Attributes:
+            first_name(str): first name of student
+            last_name(str): last name of student
+            age(int): age of student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initializes values for all instances of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            retrieves dict representation of instances
+        """
+        if isinstance(attrs, list) and all(
+                isinstance(s, str) for s in attrs):
+            return {el: getattr(self, el) for el in attrs if hasattr(self, el)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+            replaces all attributes of Student instance
+        """
+        if json:
+            for key, value in json.items():
+                self.key = value
