66089b746dc9c493fbf6d27a7d9aaaab3d706fc6
JudgeFudge19 <dremukare@gmail.com>
2021-06-01 18:23:49 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" 7-add_item: def add_item """
+""" loads content from a file and saves it in other file """
 import sys
 
 if __name__ == '__main__':
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = 
+    load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
 try:
