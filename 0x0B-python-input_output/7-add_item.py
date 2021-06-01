45e9ac5338c049d47287a30af7e23d9b4b0df36f
JudgeFudge19 <dremukare@gmail.com>
2021-06-01 18:35:11 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-""" loads content from a file and saves it in other file """
-import sys
+"""
+    module contains:
+        script that adds all arguments to a Python list
+        and then saves them to a file
+"""
+import sys.argv
 
 if __name__ == '__main__':
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -11,5 +15,5 @@ if __name__ == '__main__':
         content = load_from_json_file('add_item.json')
     except FileNotFoundError:
         content = []
-    content.extend(sys.argv[1:])
+    content.extend(argv[1:])
     save_to_json_file(content, 'add_item.json')
