0c2afee763173056d1efa1aff43f7ab59f4ae748
JudgeFudge19 <dremukare@gmail.com>
2021-06-03 09:58:21 +0000

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" base: class Base """
+
+
+class Base:
+    """
+        class Base
+        Attributes:
+            __nb_objects(int): number of objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            initializes all instances
+        """
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
