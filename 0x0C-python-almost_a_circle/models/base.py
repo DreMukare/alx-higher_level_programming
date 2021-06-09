cbcb4f1b0f1030c330557e21b8006ce86e2af0d5
JudgeFudge19 <dremukare@gmail.com>
2021-06-09 13:17:38 +0000

    returns instance with all attributes already set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,7 +53,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """ returns an instance with all attributes already set """
-        if cls.__name__ == 'Rectangle'
+        if cls.__name__ == 'Rectangle':
             dummy_obj = cls(1, 1)
         if cls.__name__ == 'Square':
             dummy_obj = cls(1)
