c7c9fcfd1cb1a3389bb67380e705c35fca078b06
Andrew Mukare <dremukare@gmail.com>
2021-06-09 14:36:08 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,48 @@
 #!/usr/bin/python3
-""" square: class Square """
+"""
+Module Square
+Inherits from class Rectangle,
+That inherits from class Base
+"""
+
+
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
-    """
-        Attributes:
-            size(int): size of square
-            x(int): x coordinate
-            y(int): y coordinate
-            id
+    """class Square inherits from class Rectangle
+    Methods:
+        def __init__(self,size,x,y,id)
+        def __str__(self)
+        def update(self, *args, **kwargs)
+        def to_dictionary(self)
+    Getter:
+       def size(self)
+    Setter:
+        def size(self, value)
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """ initializes values for all instances of Square """
-        super.__init__(size, size, x, y, id)
+        """Initialization
+        calls the supper class rectangle
+        and assigns width and height to size
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-        """ getter for Square size """
+        """Size getter"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ setter for Square size """
+        """size setter"""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """ string representation of Square instance """
+        """Overrides to return
+        "[Square] (<id>) <x>/<y> - <size>"
+        """
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.size))
-
-    def update(self, *args, **kwargs):
-        """ updates values for instances of Square """
-        if args is not None:
-            self.id = arg[0]
-            self.size = arg[1]
-            self.x = arg[2]
-            self.y = arg[3]
-        else:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-
-    def to_dictionary(self):
-        """ returns dictionary representation of Square instance """
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-            }
