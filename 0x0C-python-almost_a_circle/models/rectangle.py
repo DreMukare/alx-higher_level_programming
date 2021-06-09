f19a77d2be22eaacfa05567ee20ae861298e62a1
JudgeFudge19 <dremukare@gmail.com>
2021-06-09 09:38:59 +0000

    changed validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,25 +10,9 @@ class Rectangle(Base):
         """ Instantiation of instance attributes"""
 
         super().__init__(id)
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width <= 0
-            raise ValueError("width must be > 0")
         self.__width = width
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
         self.__height = height
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
         self.__x = x
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
         self.__y = y
 
     @property
