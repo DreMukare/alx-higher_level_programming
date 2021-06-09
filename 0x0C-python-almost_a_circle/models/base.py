5b1ab172ba85b02458c9d9f7d1105e0e37cf28fb
JudgeFudge19 <dremukare@gmail.com>
2021-06-09 12:23:05 +0000

    writes JSON string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,8 +36,9 @@ class Base:
         """ writes the JSON representation of list_objs to a file """
         filename = cls.__name__ + '.json'
         list_of_dicts = []
-        for item in list_objs:
-            list_of_dicts.append(item.to_dictionary())
+        if list_objs is not None:
+            for item in list_objs:
+                list_of_dicts.append(cls.to_dictionary(item))
         jsonstr = cls.to_json_string(list_of_dicts)
         with open(filename, 'w') as f:
             f.write(jsonstr)
