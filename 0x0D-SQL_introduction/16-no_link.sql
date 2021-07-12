f9768c65f23a22c70ba5af83951509bae9fc1a16
JudgeFudge19 <dremukare@gmail.com>
2021-07-12 19:03:37 +0000

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- lists all the records with a name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
