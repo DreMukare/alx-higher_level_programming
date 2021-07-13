3d064e1110f8d98d749db78603a2c7315a804dd4
Andrew Mukare <dremukare@gmail.com>
2021-07-13 20:19:07 +0300

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- lists all privileges of users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost',' user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
