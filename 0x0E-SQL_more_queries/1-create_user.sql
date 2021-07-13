e04fa67e106b73524c86c5d390b6e4eb0cbeb29c
JudgeFudge19 <dremukare@gmail.com>
2021-07-13 07:25:59 +0000

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates user user_0d_1 and grants all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
