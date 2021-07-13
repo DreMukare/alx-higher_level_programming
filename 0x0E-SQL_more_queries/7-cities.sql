df5a97987b47e8dc7994a9072e5690094967f377
JudgeFudge19 <dremukare@gmail.com>
2021-07-13 08:27:18 +0000

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
+USE 'hbtn_0d_usa';
+CREATE TABLE IF NOT EXISTS 'cities' (
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	`state_id` INT NOT NULL FOREIGN KEY REFERENCES states(`id`),
+	`name` VARCHAR(256) NOT NULL
+);
