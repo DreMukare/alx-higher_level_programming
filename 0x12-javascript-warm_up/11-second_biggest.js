993208db492dfe7f55e72f4ad038f8716b17a6e5
JudgeFudge19 <dremukare@gmail.com>
2021-08-02 17:46:09 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+let sortedArray = [...process.argv];
+sortedArray.splice(0, 2);
+let tempVar;
+if (process.argv.length <= 3) {
+  console.log(1);
+} else {
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < sortedArray.length - i - 1; j++) {
+      if (parseInt(sortedArray[j], 10) > parseInt(sortedArray[j + 1], 10)) {
+        tempVar = parseInt(sortedArray[j], 10);
+        sortedArray[j] = parseInt(sortedArray[j + 1], 10);
+        sortedArray[j + 1] = tempVar;
+      }
+    }
+  }
+}
+const output = sortedArray[sortedArray.length - 2]
+console.log(output);
