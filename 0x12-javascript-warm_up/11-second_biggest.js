c200e744c9f17a740c8ab62d7e066e6fc5810109
JudgeFudge19 <dremukare@gmail.com>
2021-08-02 18:08:25 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log(1);
+  console.log(0);
 } else {
-  const output = process.argv.slice(2).map(Number).sort().reverse();
+  const output = process.argv.map(Number).slice(2).sort((a, b) => a - b).reverse();
   console.log(output[1]);
 }
