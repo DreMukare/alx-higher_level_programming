2e84fa79042b6d7df64a524cf0380eb8ea034ef0
JudgeFudge19 <dremukare@gmail.com>
2021-08-03 18:46:04 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const Square = require('./5-square.js');
+const Base = require('./5-square.js');
 
-module.exports = class Square extends Square {
+module.exports = class Square extends Base {
   charPrint (c) {
-    if (c === undefined) this.print();
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
