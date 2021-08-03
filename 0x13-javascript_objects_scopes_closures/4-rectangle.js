4ddb0d98142c35374d9d073a325b74d48f287a2f
JudgeFudge19 <dremukare@gmail.com>
2021-08-03 17:30:06 +0000

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const rect = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
