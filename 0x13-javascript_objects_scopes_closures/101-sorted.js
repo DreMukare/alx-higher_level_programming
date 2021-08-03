52f6d5f9296f9f4f77894c83ea8e652496157e54
JudgeFudge19 <dremukare@gmail.com>
2021-08-03 19:52:20 +0000

    advanced task 1

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const dict = require('./101-data.js');
+const dict = require('./101-data.js').dict;
 const output = {
-  1: Object.keys(dict.dict).filter(key => dict.dict[key] === 1),
-  2: Object.keys(dict.dict).filter(key => dict.dict[key] === 2),
-  3: Object.keys(dict.dict).filter(key => dict.dict[key] === 3)
+  1: Object.keys(dict).filter(key => dict[key] === 1),
+  2: Object.keys(dict).filter(key => dict[key] === 2),
+  3: Object.keys(dict).filter(key => dict[key] === 3)
 };
 console.log(output);
