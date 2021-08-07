ce8248451663a9417bb1c1aaeb92788647a003d7
JudgeFudge19 <dremukare@gmail.com>
2021-08-07 18:04:52 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,17 +7,16 @@ state where in db hbtn_0e_usa where name matches arg
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
     cur = db.cursor()
-    squery = "SELECT * FROM states WHERE name LIKE '{:s}'\
-    ORDER BY id ASC".format(argv[4])
+    squery = "SELECT * FROM states WHERE name LIKE\
+    '{:s}' ORDER BY id ASC".format(argv[4])
     cur.execute(squery)
-    for row in cur.fetchall():
-        print(row)
+    for data in cur.fetchall():
+        print(data)
     db.close()
