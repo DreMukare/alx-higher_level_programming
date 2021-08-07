122588692a776148a44cc6e49df694a576094da9
JudgeFudge19 <dremukare@gmail.com>
2021-08-07 18:38:52 +0000

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+takes in an arg and displays all values in the table
+state where in db hbtn_0e_usa where name matches arg
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE\
+    %(state_name)s ORDER BY id ASC", {'state_name': argv[4]})
+    for data in cur.fetchall():
+        if data[1] == argv[4]:
+            print(data)
+    db.close()
