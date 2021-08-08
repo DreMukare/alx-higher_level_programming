d14ad244aa236c57a7bfbd232024e363dbadcba0
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 14:12:06 +0000

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import select, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
