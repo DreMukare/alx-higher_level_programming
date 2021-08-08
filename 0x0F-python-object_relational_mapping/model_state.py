61fa19cea2bfda5eb4961a423704fdc9ee9ec796
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 12:31:12 +0000

    tasl 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,20 +4,21 @@ contains class definition of a state and an instance
 Base = declarative_base()
 '''
 
-from sqlalchemy import create_engine,
-                       Column,
-                       Integer,
-                       String
-from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine,
+Column,
+Integer,
+String
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            argv[1],
-            argv[2],
-            argv[3],)
-            pool_pre_ping=True)
+        argv[1],
+        argv[2],
+        argv[3],)
+        pool_pre_ping=True)
     Base = declarative_base()
+
     class State(Base):
         '''
         maps to table states
