7f075d9032e9b8c781c0916b4f2f5673bd588d5c
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 15:54:03 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
+'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%')).all():
+        session.delete(state)
+        session.commit()
+    session.close()
