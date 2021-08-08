845fea1644a10443e170a0130154ccf33192b09f
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 14:16:40 +0000

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+lists all State objects from the database hbtn_0e_6_usa
+'''
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
