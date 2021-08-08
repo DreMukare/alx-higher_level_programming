e40c3c3146ad94421aca795cb4df0b7782685c1e
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 15:12:07 +0000

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).filter(
+            State.name == argv[4]).first()
+    if res is not None:
+        print(state.id)
+    else:
+        print('Not found')
+    session.close()
