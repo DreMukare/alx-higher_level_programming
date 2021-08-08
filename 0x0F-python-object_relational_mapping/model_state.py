daa3f66a41c773f88755b9c971fd318edf839a56
JudgeFudge19 <dremukare@gmail.com>
2021-08-08 13:17:52 +0000

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,29 +5,15 @@ Base = declarative_base()
 '''
 
 from sys import argv
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (create_engine,
-    Column,
-    Integer,
-    String)
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3],)
-        pool_pre_ping=True)
-    Base = declarative_base()
+Base = declarative_base()
 
-    class State(Base):
-        '''
-        maps to table states
-        '''
-        __tablename__ = 'states'
-        id = Column(Integer,
-                    autoincrement=True,
-                    unique=True,
-                    nullable=False,
-                    primary_key=True)
-        name = Column(String(128), nullable=False)
-    Base.metadata.create_all(bind=engine)
+class State(Base):
+    '''
+    maps to table states
+    '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
