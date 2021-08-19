2ad048a1a086586970979dce520c97c1b603209a
Andrew Mukare <dremukare@gmail.com>
2021-08-20 01:20:21 +0300

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a POST request and displays body of response
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be there for PLD" "$1"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
