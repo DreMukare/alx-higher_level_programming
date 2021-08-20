5d9559fb481ec409ea0cf1992f5b6caa116d3194
Andrew Mukare <dremukare@gmail.com>
2021-08-20 19:51:26 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 ''' fetches https://intranet.hbtn.io/status '''
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
     with urlopen(url) as result:
         output = result.read()
         print('Body response:')
-        print('\t - type: {}'.format(type(output)))
-        print('\t - content: {}'.format(output))
-        print('\t - utf8 content: {}'.format(output.decode('utf-8')))
+        print('\t- type: {}'.format(type(output)))
+        print('\t- content: {}'.format(output))
+        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
