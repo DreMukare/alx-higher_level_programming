1616f89f2d0b15080b83ef74b82fb559579be9d1
Andrew Mukare <dremukare@gmail.com>
2021-08-20 23:44:07 +0000

    first advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+gets 10 latest commits
+
+usage: ./100-github_commits.py <username> <rails>
+'''
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[1], argv[2])
+    res = requests.get(url)
+    output = res.json()
+    try:
+        for i in range(10):
+            print('{}: {}'.format(
+                output[i].get('sha'),
+                output[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
