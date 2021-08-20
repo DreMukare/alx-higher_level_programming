59f4304e7fe9fb2d54f9b23bda89dbbca9be0ac9
Andrew Mukare <dremukare@gmail.com>
2021-08-20 20:24:06 +0000

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,6 +2,8 @@
 '''
 takes in a url and email, sends a POST request
 displays the body of the response(utf-8 decoded)
+
+Usage: ./2-post_email.py <url> <email>
 '''
 from sys import argv
 from urllib.request import urlopen, Request
@@ -10,9 +12,7 @@ from urllib.parse import urlencode
 if __name__ == '__main__':
     url = argv[1]
     email = {'email' : argv[2]}
-    data = urlencode(email)
-    data = data.encode('ascii')
+    data = urlencode(email).encode('ascii')
     req = Request(url, data)
     with urlopen(req) as res:
-        output = res.read()
-        print(output)
+        print(res.read().decode('utf-8'))
