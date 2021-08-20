536115f1dd967546dd1aede340cb85c6387dc4f2
Andrew Mukare <dremukare@gmail.com>
2021-08-20 19:40:18 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,17 @@ from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
 
-link = 'https://intranet.hbtn.io/status'
-req = Request(link)
+url = 'https://intranet.hbtn.io/status'
+req = Request(url)
 try:
     with urlopen(req) as result:
         output = result.read()
-    print(output)
+        print('Body response:')
+        print('\t - type: {}'.format(type(result.read())))
+        print('\t - content: {}'.format(output))
+        print(
+            '\t - utf* content: {}'.format(
+                'OK' if "utf-8" in str(result.info()) else 'No'))
 except HTTPError as e:
     print(e)
 except URLError as e:
