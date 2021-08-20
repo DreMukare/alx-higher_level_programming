7adbd64d35cdb47e0013ba4c1aaaa5e725c141ac
Andrew Mukare <dremukare@gmail.com>
2021-08-20 22:56:42 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+takes in a letter
+sends POST request to http://0.0.0.0:5000/search_user with letter as param
+
+usage: ./8-json_api.py <letter>
+'''
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    q = argv[1] if len(argv) > 1 else ''
+    letter = {'q': q}
+    res = requests.post('http://0.0.0.0:5000/search_user', data=letter)
+    try:
+        output = res.json()
+        if output == {}:
+            print('No result')
+        else:
+            print('[{}] {}'.format(output.get('id'), output.get('name')))
+        except ValueError:
+            print('Not a valid JSON')
