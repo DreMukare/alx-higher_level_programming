f5bbc4369e0b303fcf974760e89cfcf192140b6f
Andrew Mukare <dremukare@gmail.com>
2021-08-20 21:38:03 +0000

    task 5:

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+GET request to url given as param
+using requests module
+displays value of X-Request-Id
+'''
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    res = requests.get(argv[1])
+    print('{}'.format(res.headers['x-request-id']))
