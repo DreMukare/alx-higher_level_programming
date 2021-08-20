69dd43a08c5aea8bc8cf89c9a0082350ff1d6a96
Andrew Mukare <dremukare@gmail.com>
2021-08-20 20:36:12 +0000

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+takes in a url and sends a GET request
+displays the body of the response(utf-8 decoded)
+
+Usage: ./3-error_code.py <url>
+'''
+from sys import argv
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from urllib.error import HTTPError
+
+if __name__ == '__main__':
+    url = argv[1]
+    req = Request(url)
+    try: 
+        with urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
