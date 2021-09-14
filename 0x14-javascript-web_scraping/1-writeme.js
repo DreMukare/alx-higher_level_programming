e86fd0b563bf05433443bdf31003ec37f28d13a9
Andrew Mukare <dremukare@gmail.com>
2021-09-14 15:04:07 +0000

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,10 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 const filepath = args[0];
-const to_write = args[1];
+const toWrite = args[1];
 
-fs.writeFile(filepath, to_write, err => {
+fs.writeFile(filepath, toWrite, err => {
   if (err) {
     console.log(err);
-    return;
   }
 });
