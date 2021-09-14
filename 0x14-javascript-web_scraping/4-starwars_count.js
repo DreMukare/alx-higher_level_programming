af5cb92a598884c9bdc36885e013c23ebb988b4e
Andrew Mukare <dremukare@gmail.com>
2021-09-14 16:30:32 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,22 +8,10 @@ const request = require('request');
 const id = '18';
 const url = process.argv.slice(2)[0];
 
-// let count = 0;
 request(url, (err, res, body) => {
   if (err) console.log(err);
   const films = JSON.parse(body).results;
   const wedgeFilms = films.map(film => film.characters)
     .filter(arr => arr.find(str => str.search(id) !== -1));
   console.log(wedgeFilms.length);
-  /*
-  for (const film in films) {
-    for (const character in films[film].characters) {
-      if (films[film].characters[character].includes(id)) {
-        count++;
-        break;
-      }
-    }
-  }
-  console.log(count);
-  */
 });
