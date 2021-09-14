dafe7941926818ec95bf3206ad4836b485289411
Andrew Mukare <dremukare@gmail.com>
2021-09-14 16:24:35 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,10 +8,24 @@ const request = require('request');
 const id = '18';
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
+let count = 0;
 request(url, (err, res, body) => {
   if (err) console.log(err);
   const films = JSON.parse(body).results;
+  /*
   const wedgeFilms = films.map(film => film.characters)
     .filter(arr => arr.find(str => str.search(id) !== -1));
   console.log(wedgeFilms.length);
+  this code works and is imo more elegant
+  didn't pass checker though...
+  */
+  for (const film in films) {
+    for (const character in films[film].characters) {
+      if (films[film].characters[character].includes(id)) {
+        count++;
+        break; 
+      }
+    }
+  }
+  console.log(count);
 });
