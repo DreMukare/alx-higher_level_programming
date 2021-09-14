0aaa56fd9fb2a9c476923b98677585cfc08fa596
Andrew Mukare <dremukare@gmail.com>
2021-09-14 15:35:54 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+    prints the title of a star wars movie
+    given integer supposed to match episode number
+*/
+
+const request = require('request');
+
+const num = process.argv.slice(2)[0];
+const url = `https://swapi-api.hbtn.io/api/films/${num}`;
+
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const output = JSON.parse(body).title;
+  console.log(output);
+});
