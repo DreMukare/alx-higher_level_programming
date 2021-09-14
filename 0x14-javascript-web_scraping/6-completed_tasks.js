eca0ab9d55eee2ccc928ffb900a2842b5f6cffc1
Andrew Mukare <dremukare@gmail.com>
2021-09-14 17:26:24 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,12 +6,12 @@ const request = require('request');
 const args = process.argv.slice(2);
 const url = args[0];
 
-const obj = {};
 request(url, (err, res, body) => {
   if (err) console.log(err);
   const tasks = JSON.parse(body);
   const idsRepeated = tasks.map(task => task.userId);
   const ids = [...new Set(idsRepeated)];
+  const obj = {};
   for (const x in ids) {
     let count = 0;
     for (const y in tasks) {
@@ -23,3 +23,19 @@ request(url, (err, res, body) => {
   }
   console.log(obj);
 });
+
+/*
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const tasks = JSON.parse(body);
+  const output = {}
+
+  for (const x in tasks) {
+    if (tasks[x].completed === true) {
+      output[tasks[x].userId] = (output[tasks[x].userId] + 1) || 1;
+    }
+  }
+
+  console.log(output);
+});
+*/
