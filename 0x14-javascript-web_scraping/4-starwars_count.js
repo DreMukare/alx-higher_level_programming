375d2280d758152f76dc07cc32ef4d557d3de81f
Andrew Mukare <dremukare@gmail.com>
2021-09-14 16:07:56 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+    prints the number of movies with Wedge Antilles
+*/
+
+const request = require('request');
+
+const id = '18';
+const url = 'https://swapi-api.hbtn.io/api/films';
+
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  const films = JSON.parse(body).results;
+  const wedgeFilms = films.map(film => film.characters)
+    .filter(arr => arr.find(str => str.search(id) !== -1));
+  console.log(wedgeFilms.length);
+});
